8279c9e9888b64e82eb540675c56a5183dd5b39b
Ebubechukwu Dimobi <dimobiblaise@gmail.com>
2022-04-03 14:55:23 +0300

    add extensions+baseVC

---
+++ AfroRestaurant/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extensions.swift
+//  AfroRestaurant
+//
+//  Created by Ebubechukwu Dimobi on 03.04.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - Regular
+    public class var background: UIColor {
+        UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+}
+
